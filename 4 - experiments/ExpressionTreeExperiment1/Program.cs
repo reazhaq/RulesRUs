55fee129b010da3c63e90e5a7298bba123686362
reaz haq <reaz@bluebonnetsoftware.com>
2017-11-21 11:15:09 -0600

    experimenting with expression tree

---
+++ 4 - experiments/ExpressionTreeExperiment1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ExpressionTreeExperiment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Func<int, bool> func1 = x => x > 5;
+            Expression<Func<int, bool>> expFunc1 = x => x > 5;
+
+            Debug.WriteLine($"func1 = {func1}");
+            Debug.WriteLine($"func1(5) = {func1(5)}");
+            Debug.WriteLine($"func1(3) = {func1(3)}");
+            Debug.WriteLine($"func1(7) = {func1(7)}");
+
+            Debug.WriteLine($"expFunc1 = {expFunc1}");
+            var expFunc1Compiled = expFunc1.Compile();
+            Debug.WriteLine($"expFunc1.Compile = expFunc1Compiled = {expFunc1Compiled}");
+            Debug.WriteLine($"expFunc1Compiled(5) = {expFunc1Compiled(5)}");
+            Debug.WriteLine($"expFunc1Compiled(3) = {expFunc1Compiled(3)}");
+            Debug.WriteLine($"expFunc1Compiled(7) = {expFunc1Compiled(7)}");
+        }
+    }
+}
