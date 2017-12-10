055dd7422be4b9b9546c232b3e4a0e2a4891c0a0
reaz haq <reaz@bluebonnetsoftware.com>
2017-12-10 09:28:04 -0600

    constant rule and tests complete

--- 2 - tests/RuleEngineTests/Rules/ConstantRuleTests.cs
+++ 2 - tests/RuleEngineTests/Rules/ConstantRuleTests.cs
@@ -17,33 +17,28 @@ namespace RuleEngineTests.Rules
             _testOutputHelper = testOutputHelper;
         }
 
-        //[Theory]
-        //[InlineData("25", 25)]
-        //[InlineData("3", 3)]
-        //[InlineData("999", 999)]
-        //public void IntegerConstantRuleShowsStringValueAssigned(string value, int expectedValue)
-        //{
-        //    var constRule = new ConstantRule<int>{Value = value};
-        //    var isCompiled = constRule.Compile();
-        //    isCompiled.ShouldBeTrue();
-
-        //    constRule.Execute().ShouldBeEqualTo(expectedValue);
-        //    _testOutputHelper.WriteLine($"constRule.Execute() with init returned {constRule.Execute()}");
-        //}
-
         [Theory]
         [InlineData(typeof(string), "something", "something")]
-        [InlineData(typeof(int?), "null", null)]
         [InlineData(typeof(string), "null", null)]
         [InlineData(typeof(int), "5", 5)]
+        [InlineData(typeof(int?), "null", null)]
         [InlineData(typeof(int?), "99", 99)]
         [InlineData(typeof(decimal), "123.45", 123.45)]
+        [InlineData(typeof(decimal?), "null", null)]
+        [InlineData(typeof(decimal?), "456.78", 456.78)]
+        [InlineData(typeof(bool), "true", true)]
+        [InlineData(typeof(bool?), "null", null)]
+        [InlineData(typeof(bool?), "false", false)]
+        [InlineData(typeof(float), "1.2", 1.2)]
+        [InlineData(typeof(float?), "null", null)]
+        [InlineData(typeof(float?), "12.34", 12.34)]
         public void ConsttantRuleChangesStringAssignedValueToTypedLambda(Type constantType, string value, object expectedResult)
         {
             var constantRuleGenericType = typeof(ConstantRule<>);
             var typesToUse = new[] {constantType};
             var constantRuleOfTypeT = constantRuleGenericType.MakeGenericType(typesToUse);
             var instanceOfConstantRuleOfTypeT = Activator.CreateInstance(constantRuleOfTypeT);
+            _testOutputHelper.WriteLine($"instanceOfConstantRuleOfTypeT = {instanceOfConstantRuleOfTypeT}");
 
             var propertyInfo = instanceOfConstantRuleOfTypeT.GetType().GetProperty("Value");
             propertyInfo.ShouldNotBeNull();
@@ -51,10 +46,24 @@ namespace RuleEngineTests.Rules
             
             var compileResult = instanceOfConstantRuleOfTypeT.GetType().GetMethod("Compile").Invoke(instanceOfConstantRuleOfTypeT, null);
             compileResult.ShouldBeOfType<bool>().ShouldBeEqualTo(true);
+            _testOutputHelper.WriteLine($"compileResult = {compileResult}");
 
             var executeResult = instanceOfConstantRuleOfTypeT.GetType().GetMethod("Execute").Invoke(instanceOfConstantRuleOfTypeT, null);
-            var expectedResultTyped = Convert.ChangeType(expectedResult, constantType);
-            Assert.True(executeResult?.Equals(expectedResultTyped) ?? expectedResultTyped == null);
+            _testOutputHelper.WriteLine($"executeResult = {executeResult ?? "null"}");
+
+            object expectedTypedResult;
+            var underyingType = Nullable.GetUnderlyingType(constantType) ?? constantType;
+            if (expectedResult == null)
+            {
+                if (constantType.IsValueType && Nullable.GetUnderlyingType(constantType) != null)
+                    expectedTypedResult = null;
+                else
+                    expectedTypedResult = Convert.ChangeType(null, constantType);
+            }
+            else
+                expectedTypedResult = Convert.ChangeType(expectedResult, underyingType);
+
+            Assert.True(executeResult?.Equals(expectedTypedResult) ?? expectedTypedResult == null);
         }
     }
 }
