c12634ca8abf18c73f26c09187aef764cb850f5a
reaz haq <reaz@bluebonnetsoftware.com>
2018-03-11 18:50:56 -0500

    * update value rule

---
+++ src/RuleFactory.Tests/Factory/UpdateValueRuleFactoriesTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using RuleFactory.Factory;
+using RuleFactory.Tests.Model;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RuleFactory.Tests.Factory
+{
+    public class UpdateValueRuleFactoriesTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public UpdateValueRuleFactoriesTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void CreateUpdateValueRule()
+        {
+            var sourceRule =
+                ConstantRuleFactories.CreateConstantRuleFromPrimitiveTypeAndString("System.String", "new name");
+
+            var propValueDictionary = new Dictionary<string, object>
+            {
+                {"ObjectToUpdate", "Name"},
+                {"SourceDataRule", sourceRule}
+            };
+
+            var updateRule = UpdateValueRuleFactories.CreateUpdateValueRule<Game>(propValueDictionary);
+            var compileResult = updateRule.Compile();
+            compileResult.Should().BeTrue();
+
+            var game = new Game {Name = "some name"};
+            updateRule.UpdateFieldOrPropertyValue(game);
+            game.Name.Should().Be("new name");
+        }
+    }
+}
---
+++ src/RuleFactory/Factory/UpdateValueRuleFactories.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using RuleEngine.Rules;
+
+namespace RuleFactory.Factory
+{
+    public class UpdateValueRuleFactories
+    {
+        public static UpdateValueRule<T> CreateUpdateValueRule<T>(IDictionary<string, object> propValueDictionary)
+        {
+            if (propValueDictionary == null) return null;
+            var instance = new UpdateValueRule<T>();
+
+            if (propValueDictionary.ContainsKey("ObjectToUpdate"))
+                instance.ObjectToUpdate = propValueDictionary["ObjectToUpdate"].ToString();
+
+            if (propValueDictionary.ContainsKey("SourceDataRule"))
+                instance.SourceDataRule = (Rule) propValueDictionary["SourceDataRule"];
+
+            return instance;
+        }
+    }
+}
