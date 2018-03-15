5f497a0b64b44f08b4b2e363726d6ef22306cd9a
reaz haq <reaz@bluebonnetsoftware.com>
2018-03-15 16:38:29 -0500

    * conditional rule... from json * contains value rule... from json

---
+++ src/RuleFactory.Tests/JsonRules/ConditionalRulesJsonTests.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using FluentAssertions;
+using Newtonsoft.Json;
+using RuleEngine.Rules;
+using RuleFactory.Tests.Model;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace RuleFactory.Tests.JsonRules
+{
+    public class ConditionalRulesJsonTests
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ConditionalRulesJsonTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory]
+        [InlineData("one", "element is present in the collection")]
+        [InlineData("nine", "element is not present in the collection")]
+        public void ConditionalWithConstantRule(string valueToCheck, string expectedOutput)
+        {
+            var rule = new ConditionalFuncRule<string, string>
+            {
+                ConditionRule = new ContainsValueRule<string>
+                {
+                    EqualityComparer = StringComparer.OrdinalIgnoreCase,
+                    CollectionToSearch = { "one", "two", "three", "four", "five", "six" }
+                },
+                TrueRule = new ConstantRule<string, string> { Value = "element is present in the collection" },
+                FalseRule = new ConstantRule<string, string> { Value = "element is not present in the collection" }
+            };
+
+            var compileResult = rule.Compile();
+            compileResult.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(rule)}:{Environment.NewLine}" +
+                                        $"{rule.ExpressionDebugView()}");
+
+            var ruleResult = rule.Execute(valueToCheck);
+            _testOutputHelper.WriteLine($"expected: {expectedOutput} - actual: {ruleResult}");
+            ruleResult.Should().BeEquivalentTo(expectedOutput);
+
+            // convert to json
+            var ruleJson = JsonConvert.SerializeObject(rule, new CustomRuleJsonConverter());
+            _testOutputHelper.WriteLine($"{nameof(ruleJson)}:{Environment.NewLine}{ruleJson}");
+            // re-hydrate from json
+            var ruleFromJson = JsonConvert.DeserializeObject<Rule>(ruleJson, new CustomRuleJsonConverter());
+            compileResult = ruleFromJson.Compile();
+            compileResult.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(ruleFromJson)}:{Environment.NewLine}" +
+                                        $"{ruleFromJson.ExpressionDebugView()}");
+
+            ruleResult = ((ConditionalFuncRule<string, string>) ruleFromJson).Execute(valueToCheck);
+            _testOutputHelper.WriteLine($"expected: {expectedOutput} - actual: {ruleResult}");
+            ruleResult.Should().BeEquivalentTo(expectedOutput);
+        }
+
+        [Fact]
+        public void ConditionalRuleToUpdateName()
+        {
+            var rule = new ConditionalIfThActionRule<Game>
+            {
+                ConditionRule = new MethodCallRule<Game, bool>
+                {
+                    ObjectToCallMethodOn = "Name",
+                    MethodToCall = "Equals",
+                    Inputs = { new ConstantRule<string> { Value = "some name" }, 
+                        new ConstantRule<StringComparison> { Value = "CurrentCultureIgnoreCase" }
+                    }
+                },
+                TrueRule = new UpdateValueRule<Game>
+                {
+                    ObjectToUpdate = "Name",
+                    SourceDataRule = new ConstantRule<string> { Value = "updated name" }
+                }
+            };
+
+            var compileResult = rule.Compile();
+            compileResult.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(rule)}:{Environment.NewLine}" +
+                                        $"{rule.ExpressionDebugView()}");
+
+            var game = new Game { Name = "some name" };
+            _testOutputHelper.WriteLine($"before game.Name: {game.Name}");
+            rule.Execute(game);
+            _testOutputHelper.WriteLine($"after game.Name: {game.Name}");
+            game.Name.Should().Be("updated name");
+
+            // convert to json
+            var ruleJson = JsonConvert.SerializeObject(rule, new CustomRuleJsonConverter());
+            _testOutputHelper.WriteLine($"{nameof(ruleJson)}:{Environment.NewLine}{ruleJson}");
+            // re-hydrate from json
+            var ruleFromJson = (ConditionalIfThActionRule<Game>)JsonConvert.DeserializeObject<Rule>(ruleJson, new CustomRuleJsonConverter());
+            var compileResult2 = ruleFromJson.Compile();
+            compileResult2.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(ruleFromJson)}:{Environment.NewLine}" +
+                                        $"{ruleFromJson.ExpressionDebugView()}");
+
+            game.Name = "some name";
+            ruleFromJson.Execute(game);
+            game.Name.Should().Be("updated name");
+        }
+
+        [Fact]
+        public void ConditionalRuleLookAtOneValueUpdateAnother()
+        {
+            var rule = new ConditionalIfThActionRule<Player>
+            {
+                ConditionRule = new ValidationRule<Player>
+                {
+                    ObjectToValidate = "Country.CountryCode",
+                    OperatorToUse = "Equal",
+                    ValueToValidateAgainst = new ConstantRule<string> { Value = "ab" }
+                },
+                TrueRule = new UpdateValueRule<Player>
+                {
+                    ObjectToUpdate = "CurrentCoOrdinates.X",
+                    SourceDataRule = new ConstantRule<int> { Value = "999" }
+                }
+            };
+
+            var compileResult = rule.Compile();
+            compileResult.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(rule)}:{Environment.NewLine}" +
+                                        $"{rule.ExpressionDebugView()}");
+
+            var player = new Player
+            {
+                Country = new Country { CountryCode = "ab" },
+                CurrentCoOrdinates = new CoOrdinate { X = 1, Y = 1 }
+            };
+            rule.Execute(player);
+            player.CurrentCoOrdinates.X.Should().Be(999);
+            _testOutputHelper.WriteLine($"expected: 999 - actual: {player.CurrentCoOrdinates.X}");
+
+            // convert to json
+            var ruleJson = JsonConvert.SerializeObject(rule, new CustomRuleJsonConverter());
+            _testOutputHelper.WriteLine($"{nameof(ruleJson)}:{Environment.NewLine}{ruleJson}");
+            // re-hydrate from json
+            var ruleFromJson = (ConditionalIfThActionRule<Player>)JsonConvert.DeserializeObject<Rule>(ruleJson, new CustomRuleJsonConverter());
+            var compileResult2 = ruleFromJson.Compile();
+            compileResult2.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(ruleFromJson)}:{Environment.NewLine}" +
+                                        $"{ruleFromJson.ExpressionDebugView()}");
+
+            player.CurrentCoOrdinates.X = 1;
+            ruleFromJson.Execute(player);
+            player.CurrentCoOrdinates.X.Should().Be(999);
+        }
+
+        [Fact]
+        public void ConditionalRuleToUpdateNameToSomethingElse()
+        {
+            var rule = new ConditionalIfThElActionRule<Game>
+            {
+                ConditionRule = new MethodCallRule<Game, bool>
+                {
+                    ObjectToCallMethodOn = "Name",
+                    MethodToCall = "Equals",
+                    Inputs = { new ConstantRule<string> { Value = "some name" }, 
+                        new ConstantRule<StringComparison> { Value = "CurrentCultureIgnoreCase" }
+                    }
+                },
+                TrueRule = new UpdateValueRule<Game>
+                {
+                    ObjectToUpdate = "Name",
+                    SourceDataRule = new ConstantRule<string> { Value = "true name" }
+                },
+                FalseRule = new UpdateValueRule<Game>
+                {
+                    ObjectToUpdate = "Name",
+                    SourceDataRule = new ConstantRule<string> { Value = "false name" }
+                }
+            };
+
+            var compileResult = rule.Compile();
+            compileResult.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(rule)}:{Environment.NewLine}" +
+                                        $"{rule.ExpressionDebugView()}");
+
+            var game = new Game { Name = "some name" };
+            _testOutputHelper.WriteLine($"before game.Name: {game.Name}");
+            rule.Execute(game);
+            _testOutputHelper.WriteLine($"after game.Name: {game.Name}");
+            game.Name.Should().Be("true name");
+
+            rule.Execute(game);
+            _testOutputHelper.WriteLine($"after after game.Name: {game.Name}");
+            game.Name.Should().Be("false name");
+
+            // convert to json
+            var ruleJson = JsonConvert.SerializeObject(rule, new CustomRuleJsonConverter());
+            _testOutputHelper.WriteLine($"{nameof(ruleJson)}:{Environment.NewLine}{ruleJson}");
+            // re-hydrate from json
+            var ruleFromJson = (ConditionalIfThElActionRule<Game>)JsonConvert.DeserializeObject<Rule>(ruleJson, new CustomRuleJsonConverter());
+            var compileResult2 = ruleFromJson.Compile();
+            compileResult2.Should().BeTrue();
+            _testOutputHelper.WriteLine($"{nameof(ruleFromJson)}:{Environment.NewLine}" +
+                                        $"{ruleFromJson.ExpressionDebugView()}");
+
+            game.Name = "some name";
+            _testOutputHelper.WriteLine($"before game.Name: {game.Name}");
+            ruleFromJson.Execute(game);
+            _testOutputHelper.WriteLine($"after game.Name: {game.Name}");
+            game.Name.Should().Be("true name");
+
+            ruleFromJson.Execute(game);
+            _testOutputHelper.WriteLine($"after after game.Name: {game.Name}");
+            game.Name.Should().Be("false name");
+        }
+    }
+}
--- src/RuleFactory/RuleFactory.cs
+++ src/RuleFactory/RuleFactory.cs
@@ -23,9 +23,50 @@ namespace RuleFactory
                 case "MethodVoidCallRule`1":
                 case "MethodCallRule`2":
                     return CreateMethodVoidCallRule(boundingTypes);
+                case "ConditionalIfThActionRule`1":
+                    return CreateConditionalIfThActionRule(boundingTypes);
+                case "ConditionalIfThElActionRule`1":
+                    return CreateConditionalIfThElActionRule(boundingTypes);
+                case "ConditionalFuncRule`2":
+                    return CreateConditionalFunctionRule(boundingTypes);
+                case "ContainsValueRule`1":
+                    return CreateContainsValueRule(boundingTypes);
             }
 
             return null;
+        }
+
+        private static Rule CreateContainsValueRule(string[] boundingTypes)
+        {
+            if (boundingTypes == null || boundingTypes.Length != 1) return null;
+            return CreateRule(typeof(ContainsValueRule<>),
+                new[] { ReflectionExtensions.GetTypeFor(boundingTypes[0]) });
+        }
+
+        private static Rule CreateConditionalFunctionRule(string[] boundingTypes)
+        {
+            if (boundingTypes == null || boundingTypes.Length != 2) return null;
+            return CreateRule(typeof(ConditionalFuncRule<,>),
+                new[]
+                {
+                    ReflectionExtensions.GetTypeFor(boundingTypes[0]),
+                    ReflectionExtensions.GetTypeFor(boundingTypes[1])
+                });
+        }
+
+        private static Rule CreateConditionalIfThElActionRule(string[] boundingTypes)
+        {
+            if (boundingTypes == null || boundingTypes.Length != 1) return null;
+            return CreateRule(typeof(ConditionalIfThElActionRule<>),
+                new[] { ReflectionExtensions.GetTypeFor(boundingTypes[0]) });
+        }
+
+        private static Rule CreateConditionalIfThActionRule(string[] boundingTypes)
+        {
+            if (boundingTypes == null || boundingTypes.Length != 1) return null;
+
+            return CreateRule(typeof(ConditionalIfThActionRule<>),
+                    new[] { ReflectionExtensions.GetTypeFor(boundingTypes[0]) });
         }
 
         private static Rule CreateMethodVoidCallRule(string[] boundingTypes)
@@ -33,9 +74,13 @@ namespace RuleFactory
             if (boundingTypes == null || boundingTypes.Length < 1 || boundingTypes.Length > 2) return null;
 
             return (boundingTypes.Length == 1
-                ? CreateRule(typeof(MethodVoidCallRule<>), new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0])})
+                ? CreateRule(typeof(MethodVoidCallRule<>), new[] { ReflectionExtensions.GetTypeFor(boundingTypes[0]) })
                 : CreateRule(typeof(MethodCallRule<,>),
-                    new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0]), ReflectionExtensions.GetTypeFor(boundingTypes[1])}));
+                    new[]
+                    {
+                        ReflectionExtensions.GetTypeFor(boundingTypes[0]),
+                        ReflectionExtensions.GetTypeFor(boundingTypes[1])
+                    }));
         }
 
         public static Rule CreateConstantRule(string[] boundingTypes)
@@ -45,7 +90,11 @@ namespace RuleFactory
             return (boundingTypes.Length == 1
                 ? CreateRule(typeof(ConstantRule<>), new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0])})
                 : CreateRule(typeof(ConstantRule<,>),
-                    new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0]), ReflectionExtensions.GetTypeFor(boundingTypes[1])}));
+                    new[]
+                    {
+                        ReflectionExtensions.GetTypeFor(boundingTypes[0]),
+                        ReflectionExtensions.GetTypeFor(boundingTypes[1])
+                    }));
         }
 
         public static Rule CreateValidationRule(string[] boundingTypes)
@@ -53,9 +102,13 @@ namespace RuleFactory
             if (boundingTypes == null || boundingTypes.Length < 1 || boundingTypes.Length > 2) return null;
 
             return (boundingTypes.Length == 1
-                    ? CreateRule(typeof(ValidationRule<>), new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0])})
-                    : CreateRule(typeof(ValidationRule<,>),
-                        new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0]), ReflectionExtensions.GetTypeFor(boundingTypes[1])}));
+                ? CreateRule(typeof(ValidationRule<>), new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0])})
+                : CreateRule(typeof(ValidationRule<,>),
+                    new[]
+                    {
+                        ReflectionExtensions.GetTypeFor(boundingTypes[0]),
+                        ReflectionExtensions.GetTypeFor(boundingTypes[1])
+                    }));
         }
 
         public static Rule CreateUpdateValueRule(string[] boundingTypes)
@@ -65,7 +118,11 @@ namespace RuleFactory
             return (boundingTypes.Length == 1
                 ? CreateRule(typeof(UpdateValueRule<>), new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0])})
                 : CreateRule(typeof(UpdateValueRule<,>),
-                    new[] {ReflectionExtensions.GetTypeFor(boundingTypes[0]), ReflectionExtensions.GetTypeFor(boundingTypes[1])}));
+                    new[]
+                    {
+                        ReflectionExtensions.GetTypeFor(boundingTypes[0]),
+                        ReflectionExtensions.GetTypeFor(boundingTypes[1])
+                    }));
 
         }
 
@@ -74,7 +131,7 @@ namespace RuleFactory
             if (genericType == null || typesToBoundTo == null) return null;
             var boundedGenericType = genericType.MakeGenericType(typesToBoundTo);
             var instance = Activator.CreateInstance(boundedGenericType);
-            return (Rule) instance;
+            return (Rule)instance;
         }
     }
 }
