bd1a5a0c690dcb14ef0d5951ea6bd4a79c2f9181
reaz haq <reaz@bluebonnetsoftware.com>
2018-03-22 22:51:23 -0500

    - contains value rule factory - and unit tests

---
+++ src/RuleFactory/RulesFactory/ContainsValueRuleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using RuleEngine.Rules;
+
+namespace RuleFactory.RulesFactory
+{
+    public static class ContainsValueRuleFactory
+    {
+        public static ContainsValueRule<T> CreateContainsValueRule<T>(IList<T> collectionToSearch,
+            string equalityComparerClassName, string equalityComparerPropertyName)
+        {
+            var rule = new ContainsValueRule<T>
+            {
+                EqualityComparerClassName = equalityComparerClassName,
+                EqualityComparerPropertyName = equalityComparerPropertyName
+            };
+            if (collectionToSearch != null)
+                rule.CollectionToSearch.AddRange(collectionToSearch);
+
+            return rule;
+        }
+    }
+}
