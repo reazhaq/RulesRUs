52682a83e9fa24653f5e01ddae136c6da97aa3cc
reaz haq <reaz@bluebonnetsoftware.com>
2018-03-27 22:11:18 -0500

    - creating sample 3 - same but rules are created using factory methods

---
+++ samples/Sample3PlaceOrderRulesUsingFactory/Model/Order.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Sample3PlaceOrderRulesUsingFactory.Model
+{
+    public class Order
+    {
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
