ee12e0c61c47f01d44db67031bb9d511e19a6ffa
reaz haq <reaz@bluebonnetsoftware.com>
2018-04-29 16:52:54 -0500

    - additional unit tests

--- src/RuleEngine.Tests/Rules/SelfReturnRuleTests.cs
+++ src/RuleEngine.Tests/Rules/SelfReturnRuleTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using FluentAssertions;
 using RuleEngine.Rules;
+using RuleEngine.Tests.Model;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -45,6 +46,26 @@ namespace RuleEngine.Tests.Rules
 
             var value = rule.Get(someValue);
             value.Should().Be(someValue);
+
+            var referenceEquals = object.ReferenceEquals(someValue, value);
+            referenceEquals.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GameSelfReturn()
+        {
+            var rule = new SelfReturnRule<Game>();
+            var compileResult = rule.Compile();
+            compileResult.Should().BeTrue();
+            _testOutputHelper.WriteLine($"selfReturnRule for Game:{Environment.NewLine}" +
+                                        $"{rule.ExpressionDebugView()}");
+
+            var someGame = new Game();
+            var value = rule.Get(someGame);
+            value.Should().Be(someGame);
+
+            var referenceEquals = object.ReferenceEquals(someGame, value);
+            referenceEquals.Should().BeTrue();
         }
     }
 }
