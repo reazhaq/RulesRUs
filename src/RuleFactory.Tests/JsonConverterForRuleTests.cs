a80523096e63a8c023512090660c01b4e10a0d01
reaz haq <reaz.haq@outlook.com>
2019-12-25 12:17:45 -0600

    work in progress

--- src/RuleFactory.Tests/JsonConverterForRuleTests.cs
+++ src/RuleFactory.Tests/JsonConverterForRuleTests.cs
@@ -112,5 +112,82 @@ namespace RuleFactory.Tests
             foo.Should().BeTrue();
             _testOutputHelper.WriteLine(rule2.ExpressionDebugView());
         }
+
+        [Fact]
+        public void Test4Point1()
+        {
+            var rule = new ConstantRule<int> { Value = "55" }; ;
+            // settings will automatically be used by JsonConvert.SerializeObject/DeserializeObject
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            //string json = JsonConvert.SerializeObject(employee,
+            //    Formatting.Indented,
+            //    new KeysJsonConverter(typeof(Employee)));
+            var json = JsonConvert.SerializeObject(rule, Formatting.Indented, new JsonConverterForRule2());
+            _testOutputHelper.WriteLine(json);
+
+            var rule2 = JsonConvert.DeserializeObject<Rule>(json, new JsonConverterForRule2());
+            var foo = rule2.Compile();
+            foo.Should().BeTrue();
+            _testOutputHelper.WriteLine(rule2.ExpressionDebugView());
+        }
+
+        [Fact]
+        public void Test3Point1()
+        {
+            var rule = new ValidationRule<Game>
+            {
+                OperatorToUse = "AndAlso",
+                RuleError = new RuleError { Code = "c", Message = "m" },
+                ChildrenRules =
+                {
+                    new ValidationRule<Game>
+                    {
+                        OperatorToUse = "NotEqual",
+                        ValueToValidateAgainst = new ConstantRule<Game> {Value = "null"}
+                    },
+                    new ValidationRule<Game>
+                    {
+                        ValueToValidateAgainst = new ConstantRule<string> {Value = "null"},
+                        ObjectToValidate = "Name",
+                        OperatorToUse = "NotEqual"
+                    },
+                    new ValidationRule<Game>
+                    {
+                        ValueToValidateAgainst = new ConstantRule<int> {Value = "3"},
+                        ObjectToValidate = "Name.Length",
+                        OperatorToUse = "GreaterThan"
+                    }
+                }
+            };
+            //var settings = new JsonSerializerSettings()
+            //{
+            //    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            //    Formatting = Formatting.Indented,
+            //    NullValueHandling = NullValueHandling.Ignore
+            //};
+            //settings.Converters.Add(new JsonConverterForRule2());
+            // settings will automatically be used by JsonConvert.SerializeObject/DeserializeObject
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+            var settings = new JsonConverterForRule2();
+
+            var json = JsonConvert.SerializeObject(rule, settings);
+            _testOutputHelper.WriteLine(json);
+
+            var rule2 = JsonConvert.DeserializeObject<Rule>(json, settings);
+            var foo = rule2.Compile();
+            foo.Should().BeTrue();
+            _testOutputHelper.WriteLine(rule2.ExpressionDebugView());
+        }
     }
 }
---
+++ src/RuleFactory/JsonConverterForRule2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RuleEngine.Rules;
+
+namespace RuleFactory
+{
+    public class JsonConverterForRule2 : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            //JToken t = JToken.FromObject(value);
+
+            //if (t.Type != JTokenType.Object)
+            //{
+            //    t.WriteTo(writer);
+            //}
+            //else
+            //{
+            //    JObject o = (JObject)t;
+            //    IList<string> propertyNames = o.Properties().Select(p => p.Name).ToList();
+
+            //    o.AddFirst(new JProperty("Keys", new JArray(propertyNames)));
+
+            //    o.WriteTo(writer);
+            //}
+
+            var jToken = JToken.FromObject(value);
+            if (jToken.Type != JTokenType.Object)
+            {
+                jToken.WriteTo(writer);
+            }
+            else
+            {
+                //var jo = new JObject();
+                var jo = (JObject)jToken;
+                var valueType = value.GetType();
+                if (typeof(Rule).IsAssignableFrom(valueType))
+                {
+                    //jo.Add("RuleType", valueType.Name);
+
+                    if (valueType.IsGenericType)
+                    {
+                        var genericTypeArguments = valueType.GenericTypeArguments;
+                        if (genericTypeArguments != null)
+                        {
+                            var boundingTypes = string.Join(",", genericTypeArguments.Select(t => t.ToString()));
+                            jo.AddFirst(new JProperty("BoundingTypes", boundingTypes));
+                            //jo.Add("BoundingTypes", string.Join(",", genericTypeArguments.Select(t => t.ToString())));
+                        }
+                    }
+                    jo.AddFirst(new JProperty("RuleType", valueType.Name));
+                }
+
+                //foreach (var prop in valueType.GetProperties())
+                //{
+                //    if (prop.CanRead)
+                //    {
+                //        var propVal = prop.GetValue(value, null);
+                //        if (propVal != null)
+                //        {
+                //            jo.Add(prop.Name, JToken.FromObject(propVal, serializer));
+                //        }
+                //    }
+                //}
+
+                //foreach (var field in valueType.GetFields())
+                //{
+                //    var fieldValue = field.GetValue(value);
+                //    if (fieldValue != null)
+                //        jo.Add(field.Name, JToken.FromObject(fieldValue, serializer));
+                //}
+
+                jo.WriteTo(writer);
+            }
+
+
+
+
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jsonObject = JObject.Load(reader);
+            Debug.WriteLine($"***** jsonObject: {jsonObject}");
+
+            var target = GetRuleObject(objectType, jsonObject);
+
+            // populate the properties of the object
+            serializer.Populate(jsonObject.CreateReader(), target);
+
+            // return the object
+            return target;
+        }
+
+        private Rule GetRuleObject(Type objectType, JObject jsonObject)
+        {
+            Debug.WriteLine($"***** objectType: {objectType}");
+            Debug.WriteLine($"********* jsonObject: {jsonObject}");
+            var ruleType = jsonObject["RuleType"].ToObject<string>();
+            Debug.WriteLine($"******* ruleType: {ruleType}");
+            var boundingTypes = jsonObject["BoundingTypes"]?.ToObject<string>();
+            Debug.WriteLine($"***** boundingTypes: {boundingTypes}");
+
+            return RuleFactory.CreateRule(ruleType, boundingTypes?.Split(','));
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            var canConvert = typeof(Rule).IsAssignableFrom(objectType);
+            Debug.WriteLine($"----->objectType: {objectType}; canConvert: {canConvert}");
+            return canConvert;
+        }
+    }
+}
